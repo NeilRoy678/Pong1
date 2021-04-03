0d0d4b5d1b40495f49340451836975944d75d361
NeilRoy678 <74729526+NeilRoy678@users.noreply.github.com>
2021-04-03 17:53:48 +0400

    Update Client.py

--- Client.py
+++ Client.py
@@ -15,22 +15,22 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def read_pos(str):
+def read(str):
     str = str.split(",")
     return int(str[0]), int(str[1])
 
 
-def make_pos(tup):
+def make(tup):
     return str(tup[0]) + "," + str(tup[1])
 
 pos = [(335,475),(335,480)]
 
 def threaded_client(conn, player):
-    conn.sendall(str.encode(make_pos(pos[player])))
+    conn.sendall(str.encode(make(pos[player])))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
+            data = read(conn.recv(2048).decode())
             pos[player] = data
 
             if not data:
@@ -45,17 +45,17 @@ def threaded_client(conn, player):
                 print("Received: ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(str.encode(make(reply)))
         except:
             break
 
     print("Lost connection")
     conn.close()
 
-currentPlayer = 0
+cp = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+    start_new_thread(threaded_client, (conn, cp))
+    cp += 1
