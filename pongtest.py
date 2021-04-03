2b6fccf040ad8b2bbb2dbf9b0d5ded05fb3995d3
Neilroy678 <royneil885@gmail.com>
2021-04-03 17:34:31 +0400

    SERVER PADDLED WITH NON SERVER BALLS"

--- pongtest.py
+++ pongtest.py
@@ -10,21 +10,21 @@ class Player():
 		self.x = x
 		self.y = y
 		self.width = width
-		self.height = height
+		self.height = height 
 		self.screen_width = 1530
 		self.screen_height = 810
+		
 		#self.rect = pygame.Rect(self.x,self.y,self.height,self.width)
 		self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
 		pygame.display.set_caption("P0NG")
-		#self.ball = pygame.Rect(self.screen_width/2 - 15, self.screen_height/2 - 15, 30, 30)
+		
 		self.player1 = pygame.Rect(self.width, self.height, 10, 140)
 
 		self.bg_color = (0, 0, 0)
 		self.white = (255, 255, 255)
-		self.ball_speed_x = 6
-		self.ball_speed_y = 6
+	
 		self.player_speed = 0
-		
+
 	def player_animation(self,player_speed):
 		self.player1.y += player_speed
 		if self.player1.top <= 0:
@@ -38,27 +38,60 @@ class Player():
 		#self.opponent.screen.fill(self.player.bg_color)
 		pygame.draw.rect(self.screen, self.white,self.player1)
 	
+	
 
 class pong:
 	def __init__(self):
 		pygame.init()
-		
+		self.ball_speed_x = 6
+		self.ball_speed_y = 6
 		self.y = 0
 		self.top = 0
 		self.bottom = 0
-
+		
+		self.screen_width = 1530
+		self.screen_height = 810
+		self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+		self.player = Player(335,475,1510,335)
+		self.opponent = Player(335,475,10,335)
+		self.screen_width = 1530
+		self.screen_height = 810
+		self.bg_color = (0, 0, 0)
+		self.white = (255, 255, 255)
+	
+		#self.ball  = Player(30, 30,750, 390)
+		self.ball = pygame.Rect(750,390, 30, 30)
 		#return (self.player.top,self.player.bottom)
 		self.opponent_speed = 0
 		self.player_speed = 0
+		#self.ball = pygame.Rect(self.screen_width/2 - 15, self.screen_height/2 - 15, 30, 30)
 		self.opp()
+	def draw2(self):
+		pygame.draw.ellipse(self.screen,self.white,self.ball)
+	def ball_animation(self):
+		#global ball_speed_x, ball_speed_y, player_score, opponent_score
+		# increments the position of the ball with +ve and -ve values
+		self.ball.x += self.ball_speed_x
+		self.ball.y += self.ball_speed_y
+		# -ve speed induces backwards movement
+		if self.ball.top <= 0 or self.ball.bottom >= self.screen_height:
+			self.ball_speed_y *= -1
+		if self.ball.left <= 0:
+			self.ball_speed_x *= -1
+		if self.ball.right >= self.screen_width:
+			self.ball_speed_x *= -1
+		if self.ball.colliderect(self.player.player1) and self.ball_speed_x > 0:
+			self.ball_speed_x *= -1
+		if self.ball.colliderect(self.opponent.player1) and self.ball_speed_x < 0:
+			self.ball_speed_x *= -1	
 	def opp(self):
 		clock = pygame.time.Clock()
 		self.net = Network()
 		data = self.read_pos(self.net.getPos()) 
-		self.player = Player(335,475,1510,335)
-		self.opponent = Player(335,480,10,335)
+		
 		while True:
 			clock.tick(65)
+			self.ball_animation()
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit() 	
@@ -86,14 +119,12 @@ class pong:
 			self.player.draw1()
 		
 			self.opponent.draw1()
-			
+			self.draw2()
 			pygame.display.flip()
 			#self.opponent = Player(n,e)
 			#client.sendall(str.encode("\n".join([str(player.top), str(player.bottom)])))
 			#n, e = [int(i) for i in client.recv(2048).decode('utf-8').split('\n')]
 			
-	
-
 	
 	def make_pos(self,x,y):
 		#data = str(self.net.pos) + ":" + str(x) + "," + str(y)
